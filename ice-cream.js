4e0a0ec681ffd7894179782028761482266d551e
Jacqueline Johnstone-Gruba <jacqueline.mjg@hotmail.com>
2020-08-06 16:55:01 +0000

    ice cream array project

---
+++ ice-cream.js
@@ -0,0 +1,17 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+//adds root beer to the end
+iceCreamFlavours.push("root beer");
+
+//logs flavours with rootbeer
+console.log(iceCreamFlavours);
+
+//logs "chocolate" to the console
+console.log(iceCreamFlavours[0]);
+
+//logs "root beer" to the console
+console.log(iceCreamFlavours[5]);
+
+//logs the length of the array to the console
+console.log(iceCreamFlavours.length);
